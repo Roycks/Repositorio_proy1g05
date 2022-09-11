96e32973d94c506cc2b9597d41a9c4290f28635f
1Mrlink1 <112593533+1Mrlink1@users.noreply.github.com>
2022-09-11 17:05:06 -0300

    Add files via upload

---
+++ proy1g5/src/proy1g5/Hotel.java
@@ -0,0 +1,86 @@
+
+package proy1g5;
+
+public class Hotel {
+    private NodoHuespedes head;
+    private NodoHuespedes tail;
+    private String cod;
+    private String nombre;
+    
+    
+    public Hotel(String cod,String nombre){
+        head=new NodoHuespedes(null,null);
+        tail=head;
+        this.cod=cod;
+        this.nombre=cod;
+    }
+
+    public String getCod() {
+        return cod;
+    }
+
+    public void setCod(String cod) {
+        this.cod = cod;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public boolean buscarHuesped(String rut){
+        NodoHuespedes aux=head;
+        while(aux.getNext()!=null){
+            if(aux.getNext().getHuesped().getRut().equals(rut)){
+                return false;
+            }
+            aux=aux.getNext();
+        }
+        return true;
+    }
+    public void agregarHuesped(Huesped huesped){
+        tail.setNext(new NodoHuespedes(huesped,null));
+        tail=tail.getNext();
+    }
+    public Huesped eliminarHuesped(String rut){
+        NodoHuespedes aux=head;
+        while(aux.getNext()!=null){
+            if(aux.getNext().getHuesped().getRut().equals(rut)){
+                Huesped h=aux.getNext().getHuesped();
+                aux.setNext(aux.getNext().getNext());
+                if (aux.getNext()==null){
+                    tail=aux;
+                    return h;
+                }
+                return h;
+            }
+            aux=aux.getNext();
+        }
+        return null;
+    }
+    public double promedioEdad(){
+        int cont=0;
+        double suma=0;
+        NodoHuespedes aux=head;
+            while(aux.getNext()!=null){
+                suma=aux.getNext().getHuesped().getEdad()+suma;
+                cont++;
+                aux=aux.getNext();  
+            }
+          
+        return (suma/cont);
+ 
+    }
+    public double sumaEdad(){
+        double suma=0;
+        NodoHuespedes aux=head;
+            while(aux.getNext()!=null){
+                suma=aux.getNext().getHuesped().getEdad()+suma;
+                aux=aux.getNext();  
+            }
+            return suma;
+    } 
+    
+}
